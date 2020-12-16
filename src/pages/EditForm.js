940d8bc0e7d738b2d7b4d09243624fd96077297a
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-16 10:34:43 -0500

    first commit initial pages setup

---
+++ src/pages/EditForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class EditForm extends Component {
+  render() {
+    return(
+      <div>
+        <h1>EditForm Page!</h1>
+      </div>
+    )
+  }
+}
+export default EditForm;
---
+++ src/pages/ShowPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ShowPage extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Show Page!</h1>
+      </div>
+    )
+  }
+}
+export default ShowPage;
